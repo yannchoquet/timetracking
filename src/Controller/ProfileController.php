2b865be0b5c2af3b20db6ba658799a648e8996cc
Yann Choquet <yann.choquet@efipeek.com>
2018-07-25 00:13:20 +0200

    Profile edit

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ProfileType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+/**
+ * @Route("/admin/profile")
+ */
+class ProfileController extends Controller
+{
+
+    /**
+     * @Route("/", name="profile_index", methods="GET|POST")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $encoder
+     * @return Response
+     */
+    public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
+    {
+
+        $user = $this->getUser();
+        $form = $this->createForm(ProfileType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $newPassword = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($newPassword);
+
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('profile_index');
+        }
+
+        return $this->render('profile/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
